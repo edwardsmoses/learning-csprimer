a1f9fb76c13bc80b8047c843f9555727425d521a
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-07 04:11:22 +0100

    feat: complete the decoder and encoder of protobuf varints

--- varint/variant.py
+++ varint/variant.py
@@ -3,15 +3,25 @@ import struct
 def encode(n):
     out = []
     while n > 0:
-        part = n % 128  #TODO bitmask for possible speed
+        part = n & 0x7f
         n >>= 7
         if(n > 0):
             part += 0x80 #using hexadecimal 
         out.append(part)
     return bytes(out)
 
+def decode (varn):
+    n = 0
+    for b in reversed(varn): 
+        n <<= 7
+        n |= (b & 0x7f)
+    return n
 
-with open ('150.uint64', 'rb') as f:
-    n = struct.unpack('>Q', f.read())[0]
-    print('encoded', encode(n))
+
+cases = ['1.uint64', '150.uint64', 'maxint.uint64']
+for fname in cases: 
+    with open (fname, 'rb') as f:
+        n = struct.unpack('>Q', f.read())[0]
+        print('protobuf', fname, n, encode(n), decode(encode(n)))
+
 
