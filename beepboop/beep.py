9d269f836a420e2985263a5f4e80c2e90b32e92f
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-09 22:14:49 +0100

    feat: update the beep write to stdout

--- beepboop/beep.py
+++ beepboop/beep.py
@@ -5,7 +5,7 @@ for line in sys.stdin:
   num = int(line)
   while (num > 0): 
     print('beep') 
-    sys.stdout.write('\a')
+    sys.stdout.buffer.write(b'\x07')
     sys.stdout.flush()
     time.sleep(2)
     num -= 1;
