0e649d2ec677fdc6532630d67496a45017172349
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-09 22:23:17 +0100

    feat: pretty simple solution for reading the keystroke

--- beepboop/beep.py
+++ beepboop/beep.py
@@ -1,6 +1,13 @@
 import sys
 import time
 import curses
+import tty 
+
+tty.setcbreak(0)
+
+while True:
+   ch = sys.stdin.read(1)
+   print (ch)
 
 
 # for line in sys.stdin:
@@ -12,22 +19,3 @@ import curses
 #     time.sleep(2)
 #     num -= 1;
 
-
-def main(win):
-    win.nodelay(True)
-    key=""
-    while 1:          
-        try:  
-           # one key stroke a time               
-           key = win.getkey()         
-           win.clear()                
-           win.addstr(str(key)) 
-
-           if key == os.linesep:
-              break           
-        except Exception as e:
-           # No input   
-           pass  
-             
-
-curses.wrapper(main)
