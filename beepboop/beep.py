9869f3364bca14a90901f7421078b03ae50a0537
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-09 22:44:18 +0100

    feat: fixed terminal - restored terminal attributes after reading key storke by key stroke

--- beepboop/beep.py
+++ beepboop/beep.py
@@ -1,21 +1,28 @@
 import sys
 import time
-import curses
+import termios
 import tty 
 
 # one keystroke at a time
+tty_attrs = termios.tcgetattr(0)
 tty.setcbreak(0)
 
-while True:
-   ch = sys.stdin.read(1)
-   num = int(ch)
-   if '1' <= ch <= '9':
-      while (num > 0):
-         sys.stdout.buffer.write(b'\x07')
-         sys.stdout.flush()
-         time.sleep(2)
-         num -= 1;
-   print (ch)
+def run():
+    while True:
+        ch = sys.stdin.read(1)
+        print (ch)
+        num = int(ch)
+        if '1' <= ch <= '9':
+            while (num > 0):
+                sys.stdout.buffer.write(b'\x07')
+                sys.stdout.flush()
+                time.sleep(2)
+                num -= 1;
+
+try:
+    run()
+finally:
+    termios.tcsetattr(0, termios.TCSADRAIN, tty_attrs)
 
 
 # for line in sys.stdin:
