7449e75838e8f12ac9a17e74c34c581665a41b5c
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-09 21:13:23 +0100

    feat: did the replace hex with rgb algorithm

--- color-convert/convert.js
+++ color-convert/convert.js
@@ -2,3 +2,18 @@ var fs = require("fs");
 var cssString = fs.readFileSync("./simple.css").toString('utf-8');
 
 console.log(cssString);
+
+const replaceHexWithRgb = (hexString) => {
+
+    var hex = parseInt(hexString, 16);
+  
+    var r = (hex >> 16) & 255;
+    var g = (hex >> 8) & 255;
+    var b = hex & 255;
+
+    return `rgb (${r} ${g} ${b})`
+}
+
+console.log('result', replaceHexWithRgb('fe030a'));
+
+
