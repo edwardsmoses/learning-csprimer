728b5a398c983f60ea7fd4543277632fe586e9a4
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-09 22:20:48 +0100

    feat: read the terminal input one character one key stroke at a time

--- beepboop/beep.py
+++ beepboop/beep.py
@@ -1,13 +1,34 @@
 import sys
 import time
-
-for line in sys.stdin:
-  num = int(line)
-  while (num > 0): 
-    print('beep') 
-    sys.stdout.buffer.write(b'\x07')
-    sys.stdout.flush()
-    time.sleep(2)
-    num -= 1;
+import curses
 
 
+# for line in sys.stdin:
+#   num = int(line)
+#   while (num > 0): 
+#     print('beep') 
+#     sys.stdout.buffer.write(b'\x07')
+#     sys.stdout.flush()
+#     time.sleep(2)
+#     num -= 1;
+
+
+def main(win):
+    win.nodelay(True)
+    key=""
+    while 1:          
+        try:                 
+           key = win.getkey()         
+           win.clear()                
+           win.addstr(str(key)) 
+
+           
+
+           if key == os.linesep:
+              break           
+        except Exception as e:
+           # No input   
+           pass  
+             
+
+curses.wrapper(main)
