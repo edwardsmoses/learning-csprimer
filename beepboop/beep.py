2fceb7c829596149a14509fe905766615ca62c6d
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-09 22:33:05 +0100

    feat: beeping correctly. but terminal is broken

--- beepboop/beep.py
+++ beepboop/beep.py
@@ -3,10 +3,18 @@ import time
 import curses
 import tty 
 
+# one keystroke at a time
 tty.setcbreak(0)
 
 while True:
    ch = sys.stdin.read(1)
+   num = int(ch)
+   if '1' <= ch <= '9':
+      while (num > 0):
+         sys.stdout.buffer.write(b'\x07')
+         sys.stdout.flush()
+         time.sleep(2)
+         num -= 1;
    print (ch)
 
 
