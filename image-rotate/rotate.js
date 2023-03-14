970c13617371d8928080857f807cb245812bf531
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-14 13:25:18 +0100

    feat: reading file sizes using the header info from wiki

--- image-rotate/rotate.js
+++ image-rotate/rotate.js
@@ -3,7 +3,18 @@ var fs = require('fs');
 const buffer = fs.readFileSync("teapot.bmp")
 console.log('buffer', buffer);
 console.log('buffer length', buffer.length);
-console.log('buffer type', buffer.toString('utf8', 0, 2));
+console.log('file type', buffer.toString('utf8', 0, 2));
+console.log('file sizes', buffer.readUInt32LE(2));
+console.log('reserved', buffer.readUInt32LE(4));
+
+
+console.log('file size', (() => {
+    let size = 0;
+    [2,3,4,5].map((i) => {
+        size += buffer[i];
+    })
+    return size;
+})());
 
 
 for(let i = 0; i < 20; i++){
