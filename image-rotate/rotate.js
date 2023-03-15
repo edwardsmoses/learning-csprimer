62c5bb0e332a4704c8a6bd49c81595a93168fda4
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-15 16:58:39 +0100

    trying to read bytes of bitmap file

--- image-rotate/rotate.js
+++ image-rotate/rotate.js
@@ -1,22 +1,17 @@
 var fs = require('fs');
 
+let indexPos = 2;
+
 const buffer = fs.readFileSync("teapot.bmp")
 console.log('buffer', buffer);
 console.log('buffer length', buffer.length);
 console.log('file type', buffer.toString('utf8', 0, 2));
-console.log('file sizes', buffer.readUInt32LE(2));
-console.log('reserved', buffer.readUInt32LE(4));
+console.log('file sizes', buffer.slice(indexPos, indexPos += 4));
+console.log('reserved', buffer.slice(indexPos, indexPos += 2));
+console.log('reserved second', buffer.slice(indexPos, indexPos += 2));
+console.log('dataOffset', buffer.readUInt32LE(indexPos, indexPos += 4));
 
 
-console.log('file size', (() => {
-    let size = 0;
-    [2,3,4,5].map((i) => {
-        size += buffer[i];
-    })
-    return size;
-})());
-
-
-for(let i = 0; i < 20; i++){
+for(let i = 20; i < 60; i++){
     console.log('val', buffer[i]);
 }
