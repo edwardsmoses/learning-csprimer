19ecbccf9b47ab9946019853b88a406eff5f7309
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-15 17:17:02 +0100

    read the first two pixel values of the bitmap file

--- image-rotate/rotate.js
+++ image-rotate/rotate.js
@@ -10,9 +10,10 @@ console.log('file sizes', buffer.slice(indexPos, indexPos += 4));
 console.log('reserved', buffer.slice(indexPos, indexPos += 2));
 console.log('reserved second', buffer.slice(indexPos, indexPos += 2));
 
-const readOffset = buffer.readInt16LE(indexPos);
+const readOffset = buffer.readUInt32LE(indexPos);
 const offset = buffer.slice(indexPos, indexPos += 4);
 
 console.log('dataOffset', indexPos, offset, readOffset);
 
-console.log('pixel first readings', buffer.slice(offset, offset + 20));
+console.log('pixel first read', buffer.slice(readOffset, readOffset + 3));
+console.log('pixel second read', buffer.slice(readOffset + 3, readOffset + 6));
