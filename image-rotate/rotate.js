635651e029deb33e52156f7ed6d7e334aacaa8c0
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-15 17:15:30 +0100

    some progress with getting the correct offset value

--- image-rotate/rotate.js
+++ image-rotate/rotate.js
@@ -9,9 +9,10 @@ console.log('file type', buffer.toString('utf8', 0, 2));
 console.log('file sizes', buffer.slice(indexPos, indexPos += 4));
 console.log('reserved', buffer.slice(indexPos, indexPos += 2));
 console.log('reserved second', buffer.slice(indexPos, indexPos += 2));
-console.log('dataOffset', buffer.readUInt32LE(indexPos, indexPos += 4));
 
+const readOffset = buffer.readInt16LE(indexPos);
+const offset = buffer.slice(indexPos, indexPos += 4);
 
-for(let i = 20; i < 60; i++){
-    console.log('val', buffer[i]);
-}
+console.log('dataOffset', indexPos, offset, readOffset);
+
+console.log('pixel first readings', buffer.slice(offset, offset + 20));
