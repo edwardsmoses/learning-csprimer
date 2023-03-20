4249944d198715b08357ddbd5f11a5061e840659
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-20 12:43:13 +0100

    trying to write black pixels to a bitmap file

--- image-rotate/rotate.js
+++ image-rotate/rotate.js
@@ -9,6 +9,11 @@ console.log('file type', buffer.toString('utf8', 0, 2));
 console.log('file sizes', buffer.slice(indexPos, indexPos += 4));
 console.log('reserved', buffer.slice(indexPos, indexPos += 2));
 console.log('reserved second', buffer.slice(indexPos, indexPos += 2));
+console.log('width/height', buffer.readInt32LE(18));
+console.log('width/height', buffer.readInt32LE(22));
+
+
+
 
 const readOffset = buffer.readUInt32LE(indexPos);
 const offset = buffer.slice(indexPos, indexPos += 4);
@@ -17,3 +22,13 @@ console.log('dataOffset', indexPos, offset, readOffset);
 
 console.log('pixel first read', buffer.slice(readOffset, readOffset + 3));
 console.log('pixel second read', buffer.slice(readOffset + 3, readOffset + 6));
+
+fs.writeFile('blank_black.bmp', buffer.slice(0, 24), () => {
+    console.log('saved successfully');
+    const blankBuffer = fs.readFileSync("blank_black.bmp")
+    console.log('blankBuffer buffer', blankBuffer);
+    console.log('blankBuffer buffer length', blankBuffer.length);
+    console.log('blankBuffer file type', blankBuffer.toString('utf8', 0, 2));
+    console.log('blankBuffer width/height', blankBuffer.readInt32LE(18));
+    console.log('blankBuffer width/height', blankBuffer.readInt32LE(22));
+});
