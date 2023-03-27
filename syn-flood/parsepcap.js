22febb1fa181a614bec02cba2f085d741a125c04
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-27 16:24:53 +0100

    read the majic number and the protocol version in the header correctly

--- syn-flood/parsepcap.js
+++ syn-flood/parsepcap.js
@@ -2,7 +2,9 @@ var fs = require('fs');
 
 let indexPos = 0;
 
-const parsePCAP = (buffer) => {
+const parsePCAP = (rbuffer) => {
+
+    const buffer = Buffer.from(rbuffer);
 
     const firstByte = buffer.readUInt32LE(indexPos);
     console.log('first byte', firstByte);
@@ -14,6 +16,15 @@ const parsePCAP = (buffer) => {
     console.log('first byte', thirdByte);
 
     console.log('header bytes', buffer.slice(0, 24))
+
+    //does the File header match the Magic Number
+    console.log('Is File Byte Ordering Correct', buffer.slice(0, 4), buffer.readUInt32LE(indexPos) == 0xa1b2c3d4);
+
+    indexPos += 4; //increase index position
+
+    //read the protocol 'Major version' and 'Minor version'
+    console.log('Pcap Protocol version ' + buffer.readInt16LE(indexPos) + "." + buffer.readInt16LE(indexPos += 2))
+
 
 }
 
