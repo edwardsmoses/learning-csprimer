90399c4076e6a1f4b04d32ba451b3a47b4f0616e
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-02 21:11:34 +0100

    feat: seperated the info into different lines

--- utf8-truncate/truncate.js
+++ utf8-truncate/truncate.js
@@ -12,21 +12,27 @@ const truncate = (rBuffer) => {
     //split in lines of text .. 
     // the lines are seperated using LF ('Line feed'), and the hex code is 0A
     const lines = [];
-    
+    let line = [];
+
     buffer.map((byte) => {
-        if(byte === 0x0a){
-            console.log('line delimiter found');
+        if (byte !== 0x0a) {
+            line.push(byte);
+        } else {
+            lines.push(line);
+            line = [];
         }
     });
-    
+
+    console.log('lines', lines, lines.length);
+
 
 
 
     fs.writeFile("expected_mine", Buffer.from(buffer), () => {
-        
+
 
         console.log('wrote to expected successfully');
-        
+
     });
 
 }
