ddc3cb9b2e26e1f44b7fba5a276bd8bc248ce1d1
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-02 21:36:13 +0100

    wrote the truncated lines to file - let's see what the result is

--- utf8-truncate/truncate.js
+++ utf8-truncate/truncate.js
@@ -25,22 +25,25 @@ const truncate = (rBuffer) => {
 
 
     //map the lines, 
-    lines.map((line, index) => {
+    const newLines = lines.map((line, index) => {
         const numberOfBytesToTruncate = line[0];
         console.log('line', index, 'number', numberOfBytesToTruncate);
         
         line.splice(0, 1).splice(0, numberOfBytesToTruncate);
 
         console.log('truncated line', index, line);
+
+        return line;
     });
+
+    const expectedLine = newLines.map((line) => {
+        line.push(0x0a); //add the line delimiter.. 
+    })
 
     
 
-    fs.writeFile("expected_mine", Buffer.from(buffer), () => {
-
-
+    fs.writeFile("expected_mine", Buffer.from(expectedLine), () => {
         console.log('wrote to expected successfully');
-
     });
 
 }
