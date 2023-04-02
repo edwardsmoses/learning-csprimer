6193c1c588183971493e50cfa5a3937484aae2f3
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-02 22:14:15 +0100

    clean up and back to no truncate

--- utf8-truncate/truncate.js
+++ utf8-truncate/truncate.js
@@ -4,54 +4,12 @@ let indexPos = 2;
 
 
 
-function isHighSurrogate(codePoint) {
-  return codePoint >= 0xd800 && codePoint <= 0xdbff;
-}
-
-function isLowSurrogate(codePoint) {
-  return codePoint >= 0xdc00 && codePoint <= 0xdfff;
-}
-
-// Truncate string by size in bytes
-function truncateLine(string, byteLength) {
-    // console.log('in function', string);
-  var charLength = string.length;
-  var curByteLength = 0;
-  var codePoint;
-  var segment;
-
-  for (var i = 0; i < charLength; i += 1) {
-    codePoint = string.charCodeAt(i);
-    segment = string[i];
-
-    if (isHighSurrogate(codePoint) && isLowSurrogate(string.charCodeAt(i + 1))) {
-      i += 1;
-      segment += string[i];
-    }
-
-    // curByteLength += getLength(segment);
-
-    if (curByteLength === byteLength) {
-      return string.slice(0, i + 1);
-    }
-    else if (curByteLength > byteLength) {
-      return string.slice(0, i - segment.length + 1);
-    }
-  }
-
-  return string;
-};
-
-
 const truncate = (rBuffer) => {
 
     const buffer = Buffer.from(rBuffer);
-    // console.log("utf8", buffer);
-    // console.log('buffer length', buffer.length);
 
-
-    //split in lines of text .. 
-    // the lines are separated using LF ('Line feed'), and the hex code is 0A
+    //split in lines of text |
+    // lines are separated using LF ('Line feed'), and the hex code is 0A
     const lines = [];
     let line = [];
 
@@ -65,28 +23,14 @@ const truncate = (rBuffer) => {
     });
 
     //map the lines, 
-    const newLines = lines.map((line, index) => {
-        const numberOfBytesToTruncate = line[0];
-        // console.log('line', index, 'number', numberOfBytesToTruncate);
-
-
-        const nLine =  truncateLine(line.filter((_, i) => i != 0).toString('utf-8'), numberOfBytesToTruncate);
-        // console.log('nLine', nLine, line.filter((_, i) => i != 0).toString('utf-8'),);
-
-        // console.log('truncated line', index, line);
-
-        return Buffer.from(nLine);
+    //to truncate
+    const expectedLines = lines.map((line, index) => {
+        const truncatedLine = Buffer.from(line)
+        return [...truncatedLine, 0x0a]; //add the delimiter...
     });
 
-    // console.log('newLines', newLines);
 
-    const expectedLine = newLines.map((line) => {
-        return [...Buffer.from(line), 0x0a]
-    })
-
-
-
-    fs.writeFile("expected_mine", Buffer.from(expectedLine.flat()), () => {
+    fs.writeFile("expected_mine", Buffer.from(expectedLines.flat()), () => {
         console.log('wrote to expected successfully');
     });
 
