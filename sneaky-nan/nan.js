22edcafab8a5092e7c612afcb57ce5c115255ff6
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-11 23:46:31 +0100

    feat: couldn't do it.

--- sneaky-nan/nan.js
+++ sneaky-nan/nan.js
@@ -11,6 +11,9 @@ const viewBit = () => {
         binStr += ('00000000' + view.getUint8(i).toString(2)).slice(-8) + ' ';
     }
 
+
+    
+
     console.log(binStr);
 
 }
@@ -21,14 +24,19 @@ const conceal = (val) => {
 
     // viewBit()
 
-    console.log(  0b111_1111_1000_1000_1000_1000_1000_1000.toString(2))
+    const buf = new ArrayBuffer(8);
+
+    const view = new DataView(buf);
+    view.setFloat64(0, NaN);
+
+    console.log('nan val', view.getInt32(), NaN);
+    
+
 
     return val;
 }
 
 const reveal = (val) => {
-
-
     console.log('isNan', val, isNaN(val));
     return val;
 }
