8e6c4e032e9ab050dbac1d6cda1787fc5006a06e
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-12 00:07:09 +0100

    still not getting what is happening yet

--- sneaky-nan/nan.py
+++ sneaky-nan/nan.py
@@ -1,8 +1,18 @@
 import struct
 
 def conceal (val): 
-    print("val", val, type(val));
-    return val
+    bs = val.encode('utf8')
+    print("val", val, type(val), bs, len(bs));
+
+
+    first = b'\x7f'
+    second = (0xf8 ^ len(bs)).to_bytes(1, 'big')
+    padding = b'\0x00' * (6 - len(bs))
+    payload = bs
+    
+    print('padding', len(first), len(second), len(padding), len(first + second + padding + payload))
+    return struct.unpack('>d', first + second + padding + payload)[0]
+
 
 def reveal (val):
     print('reveal val', val, type(val));
@@ -10,5 +20,5 @@ def reveal (val):
 
 print(struct.pack('d', float('nan')))
 
-concealed = conceal("hello world");
+concealed = conceal("hello");
 print("revealed", reveal(concealed));
