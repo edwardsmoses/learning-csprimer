a32a907e2460fd655b531f9b28198067c27c94df
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-14 19:36:40 +0100

    seems there's an error in the compilation step - and we aren't linking a library file for the math import. we'll see

--- hello-world-c/hello.c
+++ hello-world-c/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <math.h>
+
+#define N 5
 
 int main(int argc, char **argv)
 {
     printf("Hello, World! \n");
+    printf("%d * %d = %d\n", N, N, N * N, square(N));
 }
