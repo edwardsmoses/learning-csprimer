f3f8cb845a026014e45543cf0212c2fc474bc8bd
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-18 19:34:33 +0100

    start of learning the right way to do this

---
+++ bitcount/bitcount-correct.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+int bitcount(int val)
+{
+
+    return -1;
+}
+
+int main()
+{
+
+    assert(bitcount(0) == 0);
+    assert(bitcount(1) == 1);
+    assert(bitcount(3) == 2);
+    assert(bitcount(8) == 1);
+    // harder case:
+    // assert(bitcount(0xffffffff) == 32);
+    printf("OK\n");
+}
