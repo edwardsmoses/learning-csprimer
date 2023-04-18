173cb8fbad05d84422b24960a85c1e8cdafc6e8a
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-18 19:18:11 +0100

    trying to get the conversion to binary from decimal

--- bitcount/bitcount.c
+++ bitcount/bitcount.c
@@ -1,12 +1,22 @@
 #include <assert.h>
 #include <stdio.h>
 
+int bitcount(int k)
+{
 
-int main() {
-    assert(bitcount(0) == 0);
-    assert(bitcount(1) == 1);
-    assert(bitcount(3) == 2);
-    assert(bitcount(8) == 1);
+    printf("%i", (k == 0 || k == 1 ? k : ((k % 2) + 10 * bitcount(k / 2))));
+
+    return 0;
+}
+
+int main()
+{
+    bitcount(3);
+
+    // assert(bitcount(0) == 0);
+    // assert(bitcount(1) == 1);
+    // assert(bitcount(3) == 2);
+    // assert(bitcount(8) == 1);
     // harder case:
     // assert(bitcount(0xffffffff) == 32);
     printf("OK\n");
