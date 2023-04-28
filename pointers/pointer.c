c8e74ae6894060f557d76bc928804c2104a947bf
Edwards Moses <edwardsmoses3@gmail.com>
2023-04-28 19:52:15 +0100

    getting the value of an array using pointers

--- pointers/pointer.c
+++ pointers/pointer.c
@@ -12,7 +12,7 @@ int main(int argc)
     arr[3] = 42;
 
     printf("array memory address %p, arr + 1 = %p\n", arr, arr + 1);
-    printf("looking up 3rd index of the array using the memory address: %d\n", arr[3]);
+    printf("looking up 3rd index of the array using the memory address: %p, %d\n", arr + 3, *(arr + 3));
 
     printf("n = %d, &n = %p, :foo, %d\n", n, p, foo);
 }
