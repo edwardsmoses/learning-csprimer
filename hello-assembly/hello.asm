51daf3bee7f5f06025a9d5e6ad23d34da0b9283b
Edwards Moses <edwardsmoses3@gmail.com>
2023-05-02 09:01:30 +0100

    it isn't printing my other variable added - why is that

--- hello-assembly/hello.asm
+++ hello-assembly/hello.asm
@@ -13,9 +13,13 @@ _start:   mov       rax, 1                  ; system call for write
           mov       rsi, missive            ; address of string to output
           mov       rdx, 13                 ; number of bytes
           syscall                           ; invoke operating system to do the write
+          mov       rsi, letter            ; address of string to output
+          mov       rdx, 13                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
           mov       rax, 60                 ; system call for exit
           xor       rdi, rdi                ; exit code 0
           syscall                           ; invoke operating system to exit
 
           section   .data
 missive:  db        "Hello, World", 10      ; note the newline at the end
+letter:  db        "I, Alien", 10      ; note the newline at the end
