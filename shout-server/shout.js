f5f3154ad3af89a249cb6c01658f3dddc6e5ffd3
Edwards Moses <edwardsmoses3@gmail.com>
2023-05-07 01:50:35 +0100

    and completed. we'regetting a response back from our server using socket and the port we wanted to bind to

--- shout-server/shout.js
+++ shout-server/shout.js
@@ -1,33 +1,8 @@
-console.log('starting');
-
 const dgram = require('node:dgram');
 
-
 const server = dgram.createSocket('udp4');
-
-server.on('error', (err) => {
-    console.error(`server error:\n${err.stack}`);
-    server.close();
-});
-
 server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-    server.send('HELLO', rinfo.port, rinfo.address);
+    console.log(`server got msg from ${rinfo.address}:${rinfo.port}`);
+    server.send(`HELLO ${msg}`, rinfo.port, rinfo.address);
 });
-
-server.on('listening', () => {
-    const address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
-});
-
-server.on('connect', () => {
-    const address = server.address();
-    console.log(`server connected to from ${address.address}:${address.port}`);
-});
-
-server.on('close', () => {
-    const address = server.address();
-    console.log(`client closed connection - ${address.address}:${address.port}`);
-});
-
 server.bind(8888);
