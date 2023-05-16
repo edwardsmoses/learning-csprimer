a1a2d4ccf812d323b788f32a17c82b2379b5eaa5
Edwards Moses <edwardsmoses3@gmail.com>
2023-05-16 12:57:01 +0100

    feat: completed: return the http header as a json response

--- http-header-server/server.js
+++ http-header-server/server.js
@@ -7,15 +7,34 @@ net.createServer(function (conn) {
     conn.on('data', function (data) {
         console.log('ECHO_SERVER: CONN: GOT DATA: ' + data);
 
+        //return a valid HTTP response with a JSON content-type and body
         conn.write(
             'HTTP/1.0 200 OK\r\n' +
             'Content-Type: application/json\r\n' +
             '\r\n'
         );
+
+
+        //parse the headers into a JSON object
+        const regex = /^(.*?):\s(.*?)$/gm;
+        const matches = data.toString().matchAll(regex);
+        const result = {};
+
+        for (const match of matches) {
+            const key = match[1];
+            const value = match[2];
+            result[key] = value;
+        }
+
+        const jsonString = JSON.stringify(result);
+
+        //return the headers.. as json 
         conn.write(
-            '{"success":"true"}\r\n' +
+            `${jsonString} \r\n` +
             '\r\n'
         );
+
+        //end the connection
         conn.end();
     });
     conn.on('end', function () {
