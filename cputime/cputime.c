0a2ecf6c22dd207178029297cec7d14ab445c405
Edwards Moses <edwardsmoses3@gmail.com>
2023-06-02 09:07:28 +0100

    log the elasped time

--- cputime/cputime.c
+++ cputime/cputime.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdint.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -7,22 +9,36 @@
 #define NUM_MALLOCS 100000
 #define MALLOC_SIZE 1000
 
+#define TOTAL_USEC(tv) (tv).tv_sec * 100000 + (tv).tv_usec
+
 // TODO define this struct
 struct profile_times
 {
   int process_id;
+  uint64_t real_usec;
 };
 
 // TODO populate the given struct with starting information
 void profile_start(struct profile_times *t)
 {
   t->process_id = getpid();
+
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  t->real_usec = TOTAL_USEC(tv);
 }
 
 // TODO given starting information, compute and log differences to now
 void profile_log(struct profile_times *t)
 {
-  printf("pid %i \n", t->process_id);
+
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+
+  uint64_t real_diff = TOTAL_USEC(tv) - t->real_usec;
+
+  printf("[pid %i] \t", t->process_id);
+  printf("real %0.03f \n", real_diff / 1000000.0);
 }
 
 int main(int argc, char *argv[])
