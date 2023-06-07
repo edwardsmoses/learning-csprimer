1fd0b7d615e58e0ce8a299c2e718275b39afb784
Edwards Moses <edwardsmoses3@gmail.com>
2023-06-07 10:40:46 +0100

    feat: reading and getting the user time and sysm time

--- cputime/cputime.c
+++ cputime/cputime.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/time.h>
+#include <sys/resource.h>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -16,6 +17,8 @@ struct profile_times
 {
   int process_id;
   uint64_t real_usec;
+  uint64_t user_usec;
+  uint64_t system_usec;
 };
 
 // TODO populate the given struct with starting information
@@ -24,8 +27,16 @@ void profile_start(struct profile_times *t)
   t->process_id = getpid();
 
   struct timeval tv;
+  struct rusage ru;
+
   gettimeofday(&tv, NULL);
+  getrusage(RUSAGE_SELF, &ru);
+
   t->real_usec = TOTAL_USEC(tv);
+
+  t->user_usec = TOTAL_USEC(ru.ru_utime);
+  t->system_usec = TOTAL_USEC(ru.ru_stime);
+
 }
 
 // TODO given starting information, compute and log differences to now
@@ -33,12 +44,24 @@ void profile_log(struct profile_times *t)
 {
 
   struct timeval tv;
+  struct rusage ru;
+
   gettimeofday(&tv, NULL);
+  getrusage(RUSAGE_SELF, &ru);
+
 
   uint64_t real_diff = TOTAL_USEC(tv) - t->real_usec;
 
+  uint64_t usec_diff = TOTAL_USEC(ru.ru_utime) - t->user_usec;
+  uint64_t sys_diff = TOTAL_USEC(ru.ru_stime) - t->system_usec;
+
+
+
   printf("[pid %i] \t", t->process_id);
-  printf("real %0.03f \n", real_diff / 1000000.0);
+  printf("real %0.03f \t", real_diff / 1000000.0);
+  printf("user %0.03f \t", usec_diff / 1000000.0);
+  printf("system %0.03f \t \n", sys_diff / 1000000.0);
+
 }
 
 int main(int argc, char *argv[])
