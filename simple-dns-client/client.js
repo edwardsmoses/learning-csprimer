5ec812250034ab6f49d262677869d071e190643a
Edwards Moses <edwardsmoses3@gmail.com>
2023-06-12 12:09:47 +0100

    chore: adding commands i'm using to see how dns queries are structured and sent

--- simple-dns-client/client.js
+++ simple-dns-client/client.js
@@ -1,5 +1,11 @@
 const net = require('node:net');
 
+/* commands i'm using:
+dig google.com - send a dns query
+tcpdump -i eth0 -w dns.pcap port 53 - capture the dns queries into a pcap file for later analysis
+tcpdump -qns 0 -X -r dns.pcap - analysing the pcap file to see the queries that were sent
+ */
+
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -9,4 +15,5 @@ readline.question(`What's the address we want to query? \t`, address => {
     console.log(`Hi ${address}!`);
     readline.close();
 });
+
 
