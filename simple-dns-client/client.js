7bd82cc0b5bafb950922bc98e417e3ca0910c642
Edwards Moses <edwardsmoses3@gmail.com>
2023-06-19 10:01:56 +0100

    chore: remove dns commands

--- simple-dns-client/client.js
+++ simple-dns-client/client.js
@@ -1,13 +1,5 @@
 const dgram = require('node:dgram');
 
-
-/* commands i'm using:
-dig google.com - send a dns query
-tcpdump -i eth0 -w dns.pcap port 53 - capture the dns queries into a pcap file for later analysis
-tcpdump -qns 0 -X -r dns.pcap - analysing the pcap file to see the queries that were sent
-tshark -r dns.pcap - another analyzer. 
- */
-
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
