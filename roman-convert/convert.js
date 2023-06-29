0f3be56bd49acd8b75bceef368283bfb2f8cc4aa
Edwards Moses <edwardsmoses3@gmail.com>
2023-06-29 09:49:56 +0100

    feat: completed the roman numeral algorithm

--- roman-convert/convert.js
+++ roman-convert/convert.js
@@ -1,7 +1,29 @@
-
+const parts = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I",
+}
 
 const convert_number_to_numeral = (val) => {
-    return "XXXIX";
+    if (val <= 0) {
+        return ""
+    }
+
+    for (const key of Object.keys(parts).sort((a, b) => b - a)) {
+        if (Number(key) <= val) {
+            return parts[key] + convert_number_to_numeral(val - Number(key));
+        }
+    }
 }
 
 
@@ -14,6 +36,7 @@ const test_cases = {
 }
 
 Object.keys(test_cases).forEach(key => {
+    console.log('result', key, convert_number_to_numeral(Number(key)));
     assert(test_cases[key] === convert_number_to_numeral(key));
 });
 
