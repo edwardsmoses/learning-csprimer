914c5a3053c833d85602eae4342dca0784f8344c
Edwards Moses <edwardsmoses3@gmail.com>
2023-07-10 09:33:40 +0100

    not right - something broke

--- proxy-basic/http_proxy.js
+++ proxy-basic/http_proxy.js
@@ -8,39 +8,9 @@ net.createServer(function (conn) {
         console.log('ECHO_SERVER: CONN: started');
     });
 
-    //connect to the proxy host,
-    //ommiting server, the 'net' connect library assumes localhost when only the port is present
-    const proxySocket = net.connect(serverPort, () => {
-        // Log the connection details
-        console.log('Client connected:', conn.remoteAddress, conn.remotePort);
-        console.log('Proxying to:', serverPort);
-
-        // Forward data from the client to the proxy host
-        conn.pipe(proxySocket);
-
-        // Forward data from the proxy host to the client
-        proxySocket.pipe(conn);
-    });
-
-    // Log any errors with the proxy connection
-    proxySocket.on('error', (error) => {
-        console.error('Proxy socket error:', error);
-        conn.end();
-    });
-
-
     // Handle data received from the client
     conn.on('data', (data) => {
 
-        //return a valid HTTP response with a JSON content-type and body
-        conn.write(
-            'HTTP/1.0 200 OK\r\n' +
-            'Content-Type: application/json\r\n' +
-            '\r\n'
-        );
-
-
-        // Parse the client request
         const request = parseRequest(data.toString());
 
         // Log the request details
@@ -48,17 +18,30 @@ net.createServer(function (conn) {
         console.log('Request URL:', request.url);
         console.log('Request Headers:', request.headers);
 
-        // Forward the modified request to the proxy host
-        proxySocket.write(data);
 
-          //return the headers.. as json 
-          conn.write(
-            `me \r\n` +
-            '\r\n'
-        );
+        const proxySocket = net.connect(serverPort, () => {
+            console.log('Client connected:', conn.remoteAddress, conn.remotePort);
+            console.log('Proxying to:', serverPort);
 
-        //end the connection
-        conn.end();
+            // Forward data from the client to the proxy host
+            conn.pipe(proxySocket);
+
+            // Forward data from the proxy host to the client
+            proxySocket.pipe(conn);
+
+            conn.end;
+
+        });
+
+
+        // Log any errors with the proxy connection
+        proxySocket.on('error', (error) => {
+            console.error('Proxy socket error:', error);
+            clientSocket.end();
+        });
+
+
+
     });
 
     // Log when the client socket is closed
@@ -71,7 +54,6 @@ net.createServer(function (conn) {
         console.error('Client socket error:', error);
         proxySocket.end();
     });
-
 
 }).listen(9999, function () {
     console.log('ECHO_SERVER STARTED');
