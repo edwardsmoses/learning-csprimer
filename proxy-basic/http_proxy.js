5f6d05d2b3bfddb8ba5d4e367b2b916f1ba1c2de
Edwards Moses <edwardsmoses3@gmail.com>
2023-07-10 09:16:08 +0100

    feat: made progress - able to parse the request and send the request to the original server

--- proxy-basic/http_proxy.js
+++ proxy-basic/http_proxy.js
@@ -1,38 +1,84 @@
 const net = require('node:net');
 
-//original server
-const serverHost = "0.0.0.0";
+//original port
 const serverPort = 9000;
-
 
 net.createServer(function (conn) {
     conn.on('ready', function () {
         console.log('ECHO_SERVER: CONN: started');
     });
 
-    conn.on('data', function (data) {
-        console.log('ECHO_SERVER: CONN: GOT DATA: ' + data);
-    });
-
-    //connect to the proxy host
-    const proxySocket = net.connect(serverHost, serverPort, () => {
+    //connect to the proxy host,
+    //ommiting server, the 'net' connect library assumes localhost when only the port is present
+    const proxySocket = net.connect(serverPort, () => {
         // Log the connection details
-        console.log('Client connected:', clientSocket.remoteAddress, clientSocket.remotePort);
-        console.log('Proxying to:', proxyHost, proxyPort);
+        console.log('Client connected:', conn.remoteAddress, conn.remotePort);
+        console.log('Proxying to:', serverPort);
 
         // Forward data from the client to the proxy host
-        clientSocket.pipe(proxySocket);
+        conn.pipe(proxySocket);
 
         // Forward data from the proxy host to the client
-        proxySocket.pipe(clientSocket);
+        proxySocket.pipe(conn);
     });
 
     // Log any errors with the proxy connection
     proxySocket.on('error', (error) => {
         console.error('Proxy socket error:', error);
-        clientSocket.end();
+        conn.end();
     });
+
+
+    // Handle data received from the client
+    conn.on('data', (data) => {
+        // Parse the client request
+        const request = parseRequest(data.toString());
+
+        // Log the request details
+        console.log('Request Method:', request.method);
+        console.log('Request URL:', request.url);
+        console.log('Request Headers:', request.headers);
+
+        // Forward the modified request to the proxy host
+        proxySocket.write(data);
+    });
+
+    // Log when the client socket is closed
+    conn.on('close', () => {
+        console.log('Client disconnected:', conn.remoteAddress, conn.remotePort);
+    });
+
+    // Log any errors with the client socket
+    conn.on('error', (error) => {
+        console.error('Client socket error:', error);
+        proxySocket.end();
+    });
+
 
 }).listen(9999, function () {
     console.log('ECHO_SERVER STARTED');
 });
+
+
+// Helper function to parse an HTTP request
+function parseRequest(requestString) {
+    const lines = requestString.split('\r\n');
+    
+    console.log('lines', lines);
+
+    const [method, url, version] = lines[0].split(' ');
+
+    const headers = {};
+    for (let i = 1; i < lines.length; i++) {
+        if (lines[i].trim() === '') break; // Headers end with an empty line
+        const [name, value] = lines[i].split(': ');
+        headers[name.toLowerCase()] = value;
+    }
+
+    return {
+        method,
+        url,
+        version,
+        headers
+    };
+}
