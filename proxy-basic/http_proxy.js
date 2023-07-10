0f3dddeabb7d0ff5ad55f251fa014c8fea273a96
Edwards Moses <edwardsmoses3@gmail.com>
2023-07-10 09:03:37 +0100

    not sure. I'm doing this right, but creating a connection to the original server

--- proxy-basic/http_proxy.js
+++ proxy-basic/http_proxy.js
@@ -1,9 +1,38 @@
 const net = require('node:net');
 
+//original server
+const serverHost = "0.0.0.0";
+const serverPort = 9000;
+
+
 net.createServer(function (conn) {
     conn.on('ready', function () {
         console.log('ECHO_SERVER: CONN: started');
     });
+
+    conn.on('data', function (data) {
+        console.log('ECHO_SERVER: CONN: GOT DATA: ' + data);
+    });
+
+    //connect to the proxy host
+    const proxySocket = net.connect(serverHost, serverPort, () => {
+        // Log the connection details
+        console.log('Client connected:', clientSocket.remoteAddress, clientSocket.remotePort);
+        console.log('Proxying to:', proxyHost, proxyPort);
+
+        // Forward data from the client to the proxy host
+        clientSocket.pipe(proxySocket);
+
+        // Forward data from the proxy host to the client
+        proxySocket.pipe(clientSocket);
+    });
+
+    // Log any errors with the proxy connection
+    proxySocket.on('error', (error) => {
+        console.error('Proxy socket error:', error);
+        clientSocket.end();
+    });
+
 }).listen(9999, function () {
     console.log('ECHO_SERVER STARTED');
 });
