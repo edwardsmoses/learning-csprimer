c6e78573f4d81f7fd396d17c19411a72c0e4f033
Edwards Moses <edwardsmoses3@gmail.com>
2023-08-09 10:44:11 +0100

    yes: overflow

--- stack-overflow/overflow.go
+++ stack-overflow/overflow.go
@@ -4,17 +4,11 @@ import (
 	"fmt"
 )
 
-var (
-	count int
-)
-
 func main() {
 	fmt.Println("Hello, World!")
 	overflow()
 }
 
 func overflow() {
-	count += 1
-	fmt.Println("Overflowing...", count)
 	overflow()
 }
