8db315f193e6e664a406dd20da85b8155422e7d8
Edwards Moses <edwardsmoses3@gmail.com>
2023-08-09 11:15:08 +0100

    not doing this right

--- stack-overflow/overflow.go
+++ stack-overflow/overflow.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 func main() {
 	fmt.Println("Hello, Overflow!")
-	overflow(0)
+	start()
 }
 
-func overflow(depth int) {
-	fmt.Println("depth:", depth, &depth)
-	overflow(depth + 1)
+func start() {
+	depth := 0
+	fmt.Println("start")
+	overflow(depth, &depth)
+}
+
+func overflow(depth int, address *int) {
+
+	fmt.Println("depth:", depth, (*int)(unsafe.Pointer(address))-(*int)(unsafe.Pointer(&depth)))
+
+	overflow(depth+1, address)
 }
