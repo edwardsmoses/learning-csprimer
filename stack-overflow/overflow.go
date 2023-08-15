f068db4a2bff47b08ffca548c857575c79896aaf
Edwards Moses <edwardsmoses3@gmail.com>
2023-08-15 11:47:57 +0100

    logging the process id

--- stack-overflow/overflow.go
+++ stack-overflow/overflow.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"unsafe"
 )
 
@@ -12,14 +13,14 @@ func main() {
 
 func start() {
 	depth := 0
-	fmt.Println("start")
+	fmt.Println("start", os.Getpid())
 	overflow(depth, &depth)
 }
 
 func overflow(depth int, address *int) {
 
 	if depth%10000 == 0 {
-		fmt.Println("frame depth:", depth, address, unsafe.Pointer(&depth))
+		fmt.Println("[pid] ", os.Getpid(), "frame depth:", depth, address, unsafe.Pointer(&depth))
 	}
 
 	overflow(depth+1, address)
