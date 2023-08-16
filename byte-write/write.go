f9c15670bf7e520f48cf692d3d7c7ddff1ca1be5
Edwards Moses <edwardsmoses3@gmail.com>
2023-08-16 13:09:45 +0100

    feat: write one byte to a file

--- byte-write/write.go
+++ byte-write/write.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello, Byte write!")
+	writeByte()
+}
+
+func writeByte() {
+	file, err := os.Create("one_byte_by_byte")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer file.Close()
+
+	byteToWrite := []byte{
+		0x53, //write S as a byte
+	}
+	_, err = file.Write(byteToWrite)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println("Wrote 1 byte to file")
 }
