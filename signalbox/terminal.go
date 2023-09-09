68bfdbf5d7edf1c8e40cf958e265f0b72c255e32
Edwards Moses <edwardsmoses3@gmail.com>
2023-09-09 12:44:42 +0100

    feat: log to the terminal when the terminal is resized - so awesomego run terminal.gogo run terminal.go

--- signalbox/terminal.go
+++ signalbox/terminal.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	fmt.Println("Hello, Terminal Resizer Listener!")
+
+	// Create a channel to receive OS signals.
+	sigChan := make(chan os.Signal, 1)
+
+	// Notify the channel of the following signals.
+	signal.Notify(sigChan, syscall.SIGWINCH)
+
+	// Block until a signal is received.
+	for {
+		select {
+		case <-sigChan:
+			fmt.Println("Terminal is resized!")
+		}
+	}
 }
