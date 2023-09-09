34cea6d2c3cb37896be2cd0c6e6e4f6d227f8aa2
Edwards Moses <edwardsmoses3@gmail.com>
2023-09-09 12:48:04 +0100

    chore: remove the sigint signal listener - and let's proceed

--- signalbox/terminal.go
+++ signalbox/terminal.go
@@ -12,22 +12,15 @@ func main() {
 
 	// Create a channel to receive OS signals.
 	sigChan := make(chan os.Signal, 1)
-	sigExit := make(chan os.Signal, 1)
 
 	// Notify the channel of the following signals.
 	signal.Notify(sigChan, syscall.SIGWINCH)
-	//notify the chanel of a SIGINT
-	signal.Notify(sigExit, syscall.SIGINT)
 
 	// Block until a signal is received.
 	for {
 		select {
 		case <-sigChan:
 			fmt.Println("Terminal is resized!")
-		}
-		select {
-		case <-sigExit:
-			fmt.Println("Exiting!")
 		}
 	}
 }
