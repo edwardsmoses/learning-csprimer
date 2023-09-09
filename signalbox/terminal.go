226c9c6a6952476eedbf96b5706583f546bc9f06
Edwards Moses <edwardsmoses3@gmail.com>
2023-09-09 13:12:57 +0100

    completed listening to grabbing the terminal resizer

--- signalbox/terminal.go
+++ signalbox/terminal.go
@@ -5,22 +5,37 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
+func getTerminalSize() (width, height int, err error) {
+	ws, err := unix.IoctlGetWinsize(int(os.Stdout.Fd()), unix.TIOCGWINSZ)
+	if err != nil {
+		return 0, 0, err
+	}
+	return int(ws.Col), int(ws.Row), nil
+}
+
 func main() {
-	fmt.Println("Hello, Terminal Resizer Listener!")
+	fmt.Println("starting...")
 
-	// Create a channel to receive OS signals.
-	sigChan := make(chan os.Signal, 1)
+	// Create a channel to receive OS signals
+	sigs := make(chan os.Signal, 1)
 
-	// Notify the channel of the following signals.
-	signal.Notify(sigChan, syscall.SIGWINCH)
+	// Notify the channel when a SIGWINCH signal is received
+	signal.Notify(sigs, syscall.SIGWINCH)
 
-	// Block until a signal is received.
+	// Infinite loop to keep the program running and listen for signals
 	for {
 		select {
-		case <-sigChan:
-			fmt.Println("Terminal is resized!")
+		case <-sigs:
+			width, height, err := getTerminalSize()
+			if err != nil {
+				fmt.Println("Error getting terminal size:", err)
+			} else {
+				fmt.Printf("Terminal resized! New dimensions: %dx%d\n", width, height)
+			}
 		}
 	}
 }
