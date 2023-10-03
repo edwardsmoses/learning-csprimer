be11317a4adfa4dc4a6d7a1910f454c91b42e31c
Edwards Moses <edwardsmoses3@gmail.com>
2023-10-03 14:34:06 +0100

    read the user input from the stdin and println it

--- simple-shell/shell.go
+++ simple-shell/shell.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
+
+func readFromTerminal() {
+	reader := bufio.NewReader(os.Stdin)
+	cmdString, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	fmt.Println("You typed: ", cmdString)
+
+}
 
 func main() {
 	fmt.Println("starting the shell...")
+
+	readFromTerminal()
 }
