3b69673f3098a8d413ed05f824bd5d0d35d6db82
Edwards Moses <edwardsmoses3@gmail.com>
2023-11-08 10:53:21 +0100

    different approach still fails

--- simple-shell/shell.go
+++ simple-shell/shell.go
@@ -46,50 +46,29 @@ func execAppSpecificCommand(cmdString string) {
 
 func execPipeline(pipeline []string) {
 	var stdin io.Reader
-
-	var cmdList []*exec.Cmd
-	var err error
+	// var stdout io.Writer = os.Stdout
 
 	for _, cmdString := range pipeline {
 		arrCommandStr := strings.Fields(cmdString)
 
 		cmd := exec.Command(arrCommandStr[0], arrCommandStr[1:]...)
 		cmd.Stderr = os.Stderr
-
-		if stdin != nil {
-			cmd.Stdin, err = cmdList[len(cmdList)-1].StdoutPipe()
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				return
-			}
-		}
+		cmd.Stdin = stdin
 
 		stdoutReader, stdoutWriter := io.Pipe()
 		cmd.Stdout = stdoutWriter
 
-		err = cmd.Start()
+		err := cmd.Start()
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			return
 		}
-
-		cmdList = append(cmdList, cmd)
-
-		go func(reader io.Reader) {
-			defer stdoutWriter.Close()
-			io.Copy(os.Stdout, reader)
-		}(stdoutReader)
 
 		stdin = stdoutReader
+		// stdout = stdoutWriter
 	}
 
-	for _, cmd := range cmdList {
-		err = cmd.Wait()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-	}
+	io.Copy(os.Stdout, stdin)
 }
 
 func main() {
