30c5716077c24aaeadcac7ad01701d07e66dacdb
Edwards Moses <edwardsmoses3@gmail.com>
2023-11-30 14:48:27 +0100

    went with mutex

--- threaded-counter/counter.c
+++ threaded-counter/counter.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <stdatomic.h>
 
 #define EACH_COUNT 100000000
 
-atomic_int counter = 0;
+int counter = 0;
+pthread_mutex_t lock;
 
 void* thread_entry(void *arg) {
     for (int i = 0; i < EACH_COUNT; i++) {
-        atomic_fetch_add_explicit(&counter, 1, memory_order_relaxed);
+        pthread_mutex_lock(&lock); // Lock the mutex before accessing counter
+        counter++;
+        pthread_mutex_unlock(&lock); // Unlock the mutex after accessing counter
     }
     return NULL;
 }
 
 int main () {
     pthread_t p1, p2;
+    pthread_mutex_init(&lock, NULL); // Initialize the mutex
 
     pthread_create(&p1, NULL, thread_entry, NULL);
     pthread_create(&p2, NULL, thread_entry, NULL);
@@ -22,7 +25,9 @@ int main () {
     pthread_join(p1, NULL);
     pthread_join(p2, NULL);
 
-    printf("Final count: %d (expected %d)\n", atomic_load(&counter), 2 * EACH_COUNT);
+    printf("Final count: %d (expected %d)\n", counter, 2 * EACH_COUNT);
+
+    pthread_mutex_destroy(&lock); // Destroy the mutex
 
     return 0;
 }
