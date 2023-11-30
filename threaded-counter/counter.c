4e219d8679bc5eb8dd325b9249e91e1f262174d3
Edwards Moses <edwardsmoses3@gmail.com>
2023-11-30 14:33:16 +0100

    add a mutex to ensure thread safe code

--- threaded-counter/counter.c
+++ threaded-counter/counter.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#define EACH_COUNT 1000000000
+#define EACH_COUNT 100000000
 
-volatile int counter = 0;
+int counter = 0;
+pthread_mutex_t lock;
 
 void* thread_entry(void *arg) {
-  for (int i = 0; i < EACH_COUNT; i++)
-    counter++;
-  return NULL;
+    for (int i = 0; i < EACH_COUNT; i++) {
+        pthread_mutex_lock(&lock); // Lock the mutex before accessing counter
+        counter++;
+        pthread_mutex_unlock(&lock); // Unlock the mutex after accessing counter
+    }
+    return NULL;
 }
-
 
 int main () {
-  pthread_t p1, p2;
-  pthread_create(&p1, NULL, thread_entry, NULL);
-  pthread_create(&p2, NULL, thread_entry, NULL);
-  pthread_join(p1, NULL);
-  pthread_join(p2, NULL);
-  printf("Final count: %d (expected %d)\n", counter, 2 * EACH_COUNT);
-}
+    pthread_t p1, p2;
+    pthread_mutex_init(&lock, NULL); // Initialize the mutex
 
+    pthread_create(&p1, NULL, thread_entry, NULL);
+    pthread_create(&p2, NULL, thread_entry, NULL);
+
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+
+    printf("Final count: %d (expected %d)\n", counter, 2 * EACH_COUNT);
+
+    pthread_mutex_destroy(&lock); // Destroy the mutex
+
+    return 0;
+}
